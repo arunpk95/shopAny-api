0ee2fa33d89b7931635eb28f36b716def9170c6a
arunpk95 <arunpk95@gmail.com>
2021-03-25 01:46:30 -0400

    updated category return

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -3,60 +3,84 @@ const CategoryModel = require('../models/category.model');
 
 exports.insert = (req, res) => {
     CategoryModel.createNewCategory(req.body)
-    .then((result) => {
-        res.status(200).send({ status: "success", category: result });
-        return;
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
-    })
+        .then((result) => {
+            CategoryModel.getCategories()
+                .then((result) => {
+                    res.status(200).send({ status: "success", categories: result });
+                    return;
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+                })
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+        })
 }
 
-exports.update = (req,res) => {
-    CategoryModel.updateCategory(req.params.id,req.body)
-    .then((result) => {
-        res.status(200).send({ status: "success", category: result });
-        return;
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
-    })
+exports.update = (req, res) => {
+    CategoryModel.updateCategory(req.params.id, req.body)
+        .then((result) => {
+            CategoryModel.getCategories()
+                .then((result) => {
+                    res.status(200).send({ status: "success", categories: result });
+                    return;
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+                })
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+        })
 }
 
-exports.getById = (req,res) => {
+exports.getById = (req, res) => {
     CategoryModel.getCategoryByCategoryId(req.params.id)
-    .then((result) => {
-        res.status(200).send({ status: "success", category: result });
-        return;
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
-    })
+        .then((result) => {
+            res.status(200).send({ status: "success", category: result });
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+        })
 }
 
 exports.delete = (req, res) => {
     CategoryModel.deleteCategory(req.params.id)
-    .then((result) => {
-        res.status(200).send({ status: "success"});
-        return;
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
-    })
+        .then((result) => {
+            CategoryModel.getCategories()
+                .then((result) => {
+                    res.status(200).send({ status: "success", categories: result });
+                    return;
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+                })
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+        })
 }
 
-exports.getCategories = (req,res) => {
+exports.getCategories = (req, res) => {
     CategoryModel.getCategories()
-    .then((result) => {
-        res.status(200).send({ status: "success", categories: result });
-        return;
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
-    })
+        .then((result) => {
+            res.status(200).send({ status: "success", categories: result });
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(201).send({ status: "failed", errors: [{ code: -1, msg: "Internal Database Error" }] });
+        })
 }
